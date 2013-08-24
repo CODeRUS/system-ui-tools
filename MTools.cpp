11c333089ef0216cdafea5334afb0a0df44c7a39
Marcin Mielniczuk <marmistrzmar@gmail.com>
2013-08-24 17:04:08 +0200

    Fixed NFC for N950

--- MTools.cpp
+++ MTools.cpp
@@ -1,15 +1,17 @@
 #include "MTools.h"
+#include <QSystemDeviceInfo>
 
 MTools::MTools(QGraphicsItem *parent):
     MWidget(parent)
 {
+    QSystemDeviceInfo devInfo;
+
     m_layout = new MLayout(this);
     m_mainLayout = new MLinearLayoutPolicy(m_layout, Qt::Horizontal);
     m_mainLayout->setHorizontalSpacing(12);
     m_mainLayout->setContentsMargins(0,0,0,0);
-
     m_mainLayout->addItem(new MToolsBluetooth(0));
-    m_mainLayout->addItem(new MToolsNFC(0));
+    if (devInfo.productName != "RM-680") m_mainLayout->addItem(new MToolsNFC(0));
     m_mainLayout->addItem(new MToolsRadio(0));
     m_mainLayout->addItem(new MToolsWLAN(0));
     m_mainLayout->addItem(new MToolsBG(0));
