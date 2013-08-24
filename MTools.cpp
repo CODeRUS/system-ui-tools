b4f30f192e8bacb19f136b032ef6c9d9e1e9995f
Marcin Mielniczuk <marmistrzmar@gmail.com>
2013-08-24 17:05:09 +0200

    Aesthetical fix

--- MTools.cpp
+++ MTools.cpp
@@ -10,6 +10,7 @@ MTools::MTools(QGraphicsItem *parent):
     m_mainLayout = new MLinearLayoutPolicy(m_layout, Qt::Horizontal);
     m_mainLayout->setHorizontalSpacing(12);
     m_mainLayout->setContentsMargins(0,0,0,0);
+
     m_mainLayout->addItem(new MToolsBluetooth(0));
     if (devInfo.productName != "RM-680") m_mainLayout->addItem(new MToolsNFC(0));
     m_mainLayout->addItem(new MToolsRadio(0));
